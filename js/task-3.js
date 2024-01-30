dcd2297b6ce75565a3ea29c9bc3ea01692b58f86
YuliiaHavrylenko1 <gavrilenko.j94@gmail.com>
2024-01-30 18:40:50 +0000

    hw7

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+
+nameInput.addEventListener('input', updateNameOutput);
+
+function updateNameOutput() {
+  const inputValue = nameInput.value.trim();
+ const nameToShow = inputValue === '' ? 'Anonymous' : inputValue;
+
+  nameOutput.textContent = nameToShow;
+}
