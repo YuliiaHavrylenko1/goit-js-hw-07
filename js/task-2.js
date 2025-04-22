60751b1afca3f0c6fdffa0e77e5e7cc2a705f478
YuliiaHavrylenko1 <gavrilenko.j94@gmail.com>
2025-04-22 19:39:45 +0100

    editedTask

--- js/task-2.js
+++ js/task-2.js
@@ -30,7 +30,7 @@ const gallery = document.querySelector('.gallery');
 
 const galleryMarkup = images
   .map(({ url, alt }) => 
-    `<li class="gallery-item"><img src = "${url}" alt="${alt}"</li>`
+    `<li class="gallery-item"><img src = "${url}" alt="${alt}"/></li>`
   )
   .join("");
   gallery.insertAdjacentHTML("beforeend", galleryMarkup);
